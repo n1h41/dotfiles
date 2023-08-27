836be5daf3b8c21e54f123147984265fb9eeea07
n1h41 <nihalninu25@gmail.com>
2023-08-28 01:40:09 +0530

    Made changes to nvim-tree settings

--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -11,5 +11,24 @@ nvim_tree.setup({
   },
   renderer = {
     root_folder_modifier = ":t",
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+  },
+  modified = {
+    enable = true,
+  },
+  view = {
+    float = {
+      enable = true,
+    }
+  },
+  actions = {
+    change_dir = {
+      enable = false,
+      restrict_above_cwd = true,
+    }
   }
 })
