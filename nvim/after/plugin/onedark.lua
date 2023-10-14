c0840521d5efea89bb0c5aeca101f728bf9e699c
n1h41 <nihalninu25@gmail.com>
2023-10-14 19:15:45 +0530

    added one dark theme

---
+++ nvim/after/plugin/onedark.lua
@@ -0,0 +1,24 @@
+local status, onedark = pcall(require, "onedark")
+
+require("onedark.palette")
+
+if (not status) then
+  return
+end
+
+onedark.setup({
+  style = "deep",
+  transparent = true,
+  toggle_style_key = "<leader>ts",
+  code_style = {
+    keywords = 'bold',
+    strings = 'none',
+    variables = 'none',
+  },
+  highlights = {
+    ["DashboardHeader"] = { fg = "#758ed3" },
+    ["DashboardShortCut"] = { fg = "white" },
+  }
+})
+
+-- onedark.load()
